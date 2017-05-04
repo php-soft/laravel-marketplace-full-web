4ad95313356459bb9d030256e02802f4d2e3da4f
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-05-04 20:24:06 +0700

    feat(app): Create Category model
    Create Category model
    
    see #23

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function type(){
+    	return $this->belongsTo('App\Type');
 }
--- database/migrations/2017_05_04_121216_create_categories_table.php
+++ database/migrations/2017_05_04_121216_create_categories_table.php
@@ -13,7 +13,7 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('countries', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->integer('type_id')->unsigned();

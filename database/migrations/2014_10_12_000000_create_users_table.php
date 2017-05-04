e976ef98bff34ce82035b05474d6cd042157a24e
vagrant <vagrant@homestead>
2017-05-04 03:41:24 +0000

    feat(users): Create user model and reset password
    Create user model and reset password
    
    see #9

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number');
+            $table->string('date_of_birth');
+            $table->string('country');
+            $table->string('address');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('avatar');
             $table->rememberToken();
             $table->timestamps();
         });

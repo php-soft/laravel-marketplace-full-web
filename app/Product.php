0261b0c6920bf232e1830dc34e9fd2edb144303c
viettien <vvtien9696@gmail.com>
2017-05-04 18:21:06 +0700

    feat(app):Create Product model
    Create Product model
    
    see #12

---
+++ app/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+	protected $fillable=['name','description','price','_token','sale','category_id','trademark_id'];
+    public function category(){
+    	return $this->belongsTo('App\Category');
+    }
+     public function shop(){
+    	return $this->belongsTo('App\Shop');
+    }
+}

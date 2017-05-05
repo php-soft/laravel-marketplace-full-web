24672ab4b333d7b35a34fd0dba3d7dc5a3290300
unknown <viettien96>
2017-05-05 23:03:22 +0700

    feat(shop_imgae): Create Shop image model
    Create Shop image model
    
    see#33

--- database/migrations/2017_05_05_155950_create_shop_image_table.php
+++ database/migrations/2017_05_05_155950_create_shop_image_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateShopImagesTable extends Migration
+class CreateShopImageTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,12 @@ class CreateShopImagesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('shop_image', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('shop_id');
+            $table->string('image');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ class CreateShopImagesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('shop_image');
     }
 }

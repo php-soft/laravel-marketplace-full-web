706562232cc5d10009065eb6283b5d8db1897e29
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-05-06 18:31:11 +0700

    feat(app): Create Category model
    Create Category model
    
    see #46

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    public function type(){
-    	return $this->belongsTo('App\Type');
+    public function type()
+    {
+        return $this->belongsTo('App\Type');
+    }
 }

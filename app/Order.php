38c328a2d022aee38be00431b14149a9b4bc3a10
ttc007 <truongthanhcong1909@gmail.com>
2017-05-08 14:44:11 +0700

    feat(app): Create Order model
    Create Order model
    
    see #

---
+++ app/Order.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'name',
+        'ship_address',
+        'phone_number',
+        'email',
+        'couuntry_id',
+        'city_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongTo('App\User');
+    }
+
+    public function country()
+    {
+        return $this->belongsTo('App\Country');
+    }
+
+    public function city()
+    {
+        return $this->belongsTo('App\City');
+    }
+}
---
+++ database/migrations/2017_05_08_072914_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->nullable();
+            $table->string('name');
+            $table->string('ship_address');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->unsignedInteger('country_id');
+            $table->unsignedInteger('city_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}

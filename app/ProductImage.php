71db8b2628353c332191e3aad2c09640de68acb4
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-05-08 14:07:29 +0700

    feat(shop): Update ProductImage model
    Update ProductImage model
    
    see #71

--- app/ProductImage.php
+++ app/ProductImage.php
@@ -11,7 +11,7 @@ class ProductImage extends Model
         'image'
     ];
 
-    public function order()
+    public function product()
     {
         return $this->belongsTo('App\Product');
     }

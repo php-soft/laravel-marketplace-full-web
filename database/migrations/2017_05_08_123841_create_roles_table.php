ae47fcef8b8f3281557f3b5809a3072b23007189
vagrant <vagrant@homestead>
2017-05-08 17:03:15 +0000

    feat(roles): Create roles table
    modified:   database/migrations/2017_05_08_123841_create_roles_table.php
    
    see #76

--- database/migrations/2017_05_08_123841_create_roles_table.php
+++ database/migrations/2017_05_08_123841_create_roles_table.php
@@ -16,7 +16,7 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }

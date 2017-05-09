1d3f3326efb8fac456f7b6e87ec81089da4537c4
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-05-09 19:05:02 +0700

    feat(shop): Add relationship productImages to Product model
    Add relationship productImages to Product model
    
    see #113

--- app/Product.php
+++ app/Product.php
@@ -17,4 +17,9 @@ class Product extends Model
     {
         return $this->belongsTo('App\Shop');
     }
+
+    public function productImages()
+    {
+        return $this->hasMany('App\ProductImage');
+    }
 }

e1d69ec8baa75158aedddfa7eeae13e45fe8dd57
vagrant <vagrant@homestead>
2017-05-09 11:22:29 +0000

    refactor(roles): Create relationship for roles table
    modified:   app/Role.php
    
    see #97

--- app/Role.php
+++ app/Role.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     protected $fillable = ['name', 'description'];
-    public function assignments(){
+    public function assignments()
+    {
     	return $this->hasMany('app\Assignment');
     }
 }

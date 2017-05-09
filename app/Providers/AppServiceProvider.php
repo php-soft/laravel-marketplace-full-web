604f99f65be616a20dbac9365b60936215585d68
BQuyetDev <nguyentruongbaoquyetit@gmail.com>
2017-05-09 11:36:36 +0700

    feat(fix):Add Schema::defaultStringLength in AppServiceProvider
    Add Schema::defaultStringLength in AppserviceProvider
    
    see #62

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**

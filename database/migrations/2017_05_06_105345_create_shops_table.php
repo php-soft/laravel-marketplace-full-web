28334e92af8393d25f23bbed8626e9a414a4a111
BQuyetDev <nguyentruongbaoquyetit@gmail.com>
2017-05-09 12:13:48 +0700

    refactor(shops):create relationship for shops table
    create ralationship for shops table
    
    see #114

--- database/migrations/2017_05_06_105345_create_shops_table.php
+++ database/migrations/2017_05_06_105345_create_shops_table.php
@@ -17,10 +17,15 @@ class CreateShopsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('type_id')->unsigned();
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('cascade');
             $table->integer('city_id')->unsigned();
+            $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
             $table->integer('district_id')->unsigned();
+            $table->foreign('district_id')->references('id')->on('districts')->onDelete('cascade');
             $table->integer('country_id')->unsigned();
+            $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade');
             $table->longText('description');
             $table->string('image');
             $table->tinyInteger('status');

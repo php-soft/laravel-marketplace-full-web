d5146658a541fa40a5e69bc6d012ec734ce40c5e
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-05-09 18:39:31 +0700

    feat(shop): Create Category Reference
    Create Category Reference
    
    see #105

--- app/Category.php
+++ app/Category.php
@@ -12,4 +12,9 @@ class Category extends Model
     {
         return $this->belongsTo('App\Type');
     }
+
+    public function product()
+    {
+        return $this->hasMany('App\Product');
+    }
 }

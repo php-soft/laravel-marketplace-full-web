0d914647e24ff0b43a65e12d9799db2dcfb0273f
npthang <npthang02959@gmail.com>
2017-05-09 22:58:13 +0700

    refactor(assignments): Create relationship for assignment table
    modifield: app/Assignment.php
    
    see #107

--- app/Assignment.php
+++ app/Assignment.php
@@ -7,8 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Assignment extends Model
 {
     protected $fillable = ['role_id', 'user_id',];
+    
     public function roles()
     {
         return $this->belongsTo('app\Role');
+    }
+
+    public function users()
+    {
+        return $this->belongsTo('app\User');
     }
 }

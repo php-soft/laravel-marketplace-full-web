38f71778f272dec56b15c0cacb6a0e204fb581a7
ttc007 <truongthanhcong1909@gmail.com>
2017-05-11 02:19:49 +0700

    feat(admin): Add Product
    Add Product function
    
    see #148

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', 'AdminController@admin');
+
+    Route::post('storeProduct', 'AdminController@storeProduct');
+    Route::post('storeCategory', 'AdminController@storeCategory');
+
+});

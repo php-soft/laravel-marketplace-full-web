eac4717be013b86374f3ed8e1ec28f328211fe64
viet tien vy <viet tien vy>
2017-05-10 08:40:50 +0700

    feat(type): Create relationship Type to Shops model
    Create relationship Type to Shops model
    
    see#120

--- app/Type.php
+++ app/Type.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
-    //
+    protected $fillable = [
+    'name'
+    ];
+    public function shop()
+    {
+    	 return $this->belongsTo('App\Shop');
+    }
 }

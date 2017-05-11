9e563045013e048840282768dd9ab960a8ed0e37
viet tien vy <viet tien vy>
2017-05-12 06:59:42 +0700

    feat(shopimage): Create relationship for shopimages table
    Create relationship for shopimages table
    
    see #135

--- app/ShopImage.php
+++ app/ShopImage.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShopImage extends Model
 {
-    //
+    protected $fillable = [
+        'shop_id',
+        'image'
+    ];
+    
+    public function shop()
+    {
+        retrun $this->belongsTo('App\Shop');
+    }
 }

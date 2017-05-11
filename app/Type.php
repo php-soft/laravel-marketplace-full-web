707db006edb544c87307f214f7db730f84c7c61d
viet tien vy <viet tien vy>
2017-05-11 11:02:17 +0700

    feat(type): Create relationship for types table
    Create relationship for types table
    
    see #137

--- app/Type.php
+++ app/Type.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
-    protected $fillable = [
-    'name'
-    ];
-
+    public function categories()
+    {
+        return $this->hasMany('App\Category');
+    }
     public function shops()
     {
         return $this->hasMany('App\Shop');

c3a7e4ab7ce3bfbe3f0e9e6b9663648dff405119
npthang <npthang02959@gmail.com>
2017-05-11 19:38:35 +0700

    see #157

--- app/Http/Controllers/Admin/CountryController.php
+++ app/Http/Controllers/Admin/CountryController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Country
+use App\Country;
 use Illuminate\Http\Request;
 
 class CountryController extends Controller

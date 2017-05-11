ee2a08feb03b6388f6caa1c4f0f99dcc4e5d4487
ttc007 <truongthanhcong1909@gmail.com>
2017-05-11 22:57:31 +0700

    feat(admin): Admin create Product
    Admin create Product
    
    see #168
    $ git

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -25,18 +25,18 @@ class ProductController extends Controller
         $categories = Category::pluck('name', 'id');
         $shops = Shop::pluck('name', 'id');
         return view('admin.products.create')
-            ->with('categories',$categories)
-            ->with('shops',$shops);
+            ->with('categories', $categories)
+            ->with('shops', $shops);
     }
 
     public function storeProduct()
     {
         $image=Input::get('image');
         $file=Input::file('image');
-        if(!empty($file)) {
+        if(!empty($file)){
             $destinationPath = 'images';
             Input::file('image')
-                ->move($destinationPath,$image.'.png');
+                ->move($destinationPath, $image.'.png');
         }
 
         Product::create(Input::all());

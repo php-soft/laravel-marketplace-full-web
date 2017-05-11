13e636547d644fb897d757f4cc0e1309cfbaefb1
viettien <vvtien9696@gmail.com>
2017-05-11 19:08:18 +0700

    feat(admin): Create admin page
    Create  Producs admin page
    
    see #164

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,13 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Input;
-use App\Type;
-use App\Category;
-use App\Product;
-use App\Shop;
 
 class AdminController extends Controller
 {

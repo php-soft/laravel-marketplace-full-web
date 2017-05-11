b52c1157c62b31999bc578ea11600a2f635d6c57
viet tien vy <viet tien vy>
2017-05-11 11:26:39 +0700

    feat(shopassignment): Create relationship for shopassignment table
    Create relationship for shopassignment table
    
    see #138

--- app/ShopAssignment.php
+++ app/ShopAssignment.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShopAssignment extends Model
 {
-    //
+    protected $fillable = [
+        'shop_id',
+        'user_id',
+        'role_id'
+    ];
+
+    public function shops()
+    {
+        return $this->belongsTo('App\Shop');
+    }
+    public function users()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function roles()
+    {
+        return $this->belongsTo('App\Role');
+    }
 }

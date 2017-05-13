f0f953dd66a1db7cf52a13e9bff330d72d6341f4
npthang <npthang02959@gmail.com>
2017-05-13 19:11:00 +0700

    feat(countries): Index page
    modified:   app/Http/Controllers/Admin/CountryController.php
    
    see #157

--- app/Http/Controllers/Admin/CountryController.php
+++ app/Http/Controllers/Admin/CountryController.php
@@ -13,9 +13,4 @@ class CountryController extends Controller
         $countries = Country::all();
         return view('admin.countries.index')->with('countries', $countries);
     }
-
-    public function country_create()
-    {
-        $countries = Country::all();
-        return view('countries.create')->with('countries', $countries);
 }

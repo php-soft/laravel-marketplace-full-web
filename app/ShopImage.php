4e8abdeb9a53fb2a5142930649c17c14c7a3c3b6
viet tien vy <viet tien vy>
2017-05-14 07:20:31 +0700

    refactor(shopimage): Update ShopImage table
    Update ShopImage table
    
    see #184

--- app/ShopImage.php
+++ app/ShopImage.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShopImage extends Model
 {
-    //
+    protected $fillable = [
+        'shop_id',
+        'image'
+    ];
+    
+    public function shop()
+    {
+        return $this->belongsTo('App\Shop');
+    }
 }

6678569db799c8a722a3af36cdc3a697eba2c27c
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-05-15 15:37:05 +0700

    feat(shop): Admin create category
    Update Admin create category
    
    see #204

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.admin')
 @section('header')
-	<h2>Add a new category</h2>
- @stop
- @section('content')
- 		{!!Form::open(['url'=>'admin/categories'])!!}
- 		@include('partials.forms.category')
- 		{!!Form::close()!!}
-  @stop
+    <h2>Add a new category</h2>
+@stop
+@section('content')
+        {!!Form::open(['url'=>'admin/categories'])!!}
+        @include('partials.forms.category')
+        {!!Form::close()!!}
+@stop
--- resources/views/partials/forms/category.blade.php
+++ resources/views/partials/forms/category.blade.php
@@ -3,12 +3,12 @@
     <div class="form-controls">
         {!!Form::text('name', null, ['class'=>'form-control'])!!}
     </div>
- </div>
+</div>
     
- <div class="form-group">
-        {!!Form::label('type_id', 'Type')!!}
-        <div class="form-controls">
-            {!!Form::select('type_id', $types, null, ['class'=>'form-control'])!!}
-        </div>
- </div>
- {!!Form::submit('Save Category', ['class'=>'btn btn-primary'])!!}
+<div class="form-group">
+    {!!Form::label('type_id', 'Type')!!}
+    <div class="form-controls">
+        {!!Form::select('type_id', $types, null, ['class'=>'form-control'])!!}
+    </div>
+</div>
+    {!!Form::submit('Save Category', ['class'=>'btn btn-primary'])!!}

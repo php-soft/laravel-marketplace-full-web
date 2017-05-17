bfd04ed0f0da0f61a06b74911b1f55662bf2e881
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-05-17 14:06:03 +0700

    feat(shop): Admin category create (#237)
    feat(shop): Admin category create
    
    Admin category create
    
    close #237

--- config/app.php
+++ config/app.php
@@ -228,6 +228,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Input' => Illuminate\Support\Facades\Input::class,
 
     ],
 

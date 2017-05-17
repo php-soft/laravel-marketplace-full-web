b27abe4df5405162a817e1ae6ab386ae4c2db3de
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-05-17 13:28:28 +0700

    feat: test erro
    tesr erro
    
    see #239

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -10,21 +10,9 @@ use Input;
 
 class CategoryController extends Controller
 {
-    public function index()
+    public function indexxx()
     {
         $categories = Category::all();
         return view('admin.categories.index')->with('categories', $categories);
-    }
-
-    public function create()
-    {
-        $types = Type::pluck('name', 'id');
-        return view('admin.categories.create')->with('types', $types);
-    }
-
-    public function store()
-    {
-        $categories=Category::create(Input::all());
-        return redirect('admin/categories');
     }
 }

a387209d6eef3387814188e394bfe5bc9c4603a1
vagrant <haytranngoc@gmail.com>
2017-05-17 09:19:19 +0000

    a

--- routes/web.php
+++ routes/web.php
@@ -37,5 +37,5 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
 
     Route::get('/cities', 'CityController@index')->name('adminCities');
     Route::get('/cities/create', 'CityController@create')->name('adminCitiesCreate');
-    Route::post('/cities', 'CityController@store')->name('adminCitiesStore');  
+    Route::post('/cities', 'CityController@store')->name('adminCitiesStore');
 });

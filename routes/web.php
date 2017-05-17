3e5b8ab7a47054ca063ce0b2ba83eb7a24f6770d
vagrant <haytranngoc@gmail.com>
2017-05-17 10:49:11 +0000

    fix error

--- routes/web.php
+++ routes/web.php
@@ -38,5 +38,4 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
     Route::get('/cities', 'CityController@index')->name('adminCities');
     Route::get('/cities/create', 'CityController@create')->name('adminCitiesCreate');
     Route::post('/cities', 'CityController@store')->name('adminCities');
-
 });

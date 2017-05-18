3e826ce207c45f74580804c8ee55ad86c45cbe62
Hay Tran <haytranngoc@gmail.com>
2017-05-18 08:39:26 +0700

    Update create.blade.php

--- resources/views/admin/cities/create.blade.php
+++ resources/views/admin/cities/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-12 ">
             <div class="panel panel-default">
-                <div class="panel-heading">Create Cities</div>
+                <div class="panel-heading">Create City</div>
 
                 <div class="panel-body">
                     {{ Form::open(['url'=>'admin/cities']) }}

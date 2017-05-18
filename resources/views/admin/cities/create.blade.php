ee817c4b32ca8c0b9768465f9207ecbade5e4a79
Hay Tran <haytranngoc@gmail.com>
2017-05-18 08:49:27 +0700

    Update create.blade.php

--- resources/views/admin/cities/create.blade.php
+++ resources/views/admin/cities/create.blade.php
@@ -16,9 +16,9 @@
                             </div>
                         </div>
                         <div class="form-group">
-                            {!!Form::label('country_id', 'Country')!!}
+                            {!! Form::label('country_id', 'Country') !!}
                             <div class="form-controls">
-                                {!!Form::select('country_id', $countries, null, ['class'=>'form-control'])!!}
+                                {!! Form::select('country_id', $countries, null, ['class'=>'form-control']) !!}
                             </div>
                         </div>
                         {!! Form::submit('Save', ['class'=>'btn btn-primary']) !!}

dc744ed72f110a5a760a48932396281675b58f7b
npthang <npthang02959@gmail.com>
2017-05-19 14:29:28 +0700

    feat(districts): District admin edit-page
    modified:   resources/views/admin/districts/index.blade.php
    
    see #271

--- resources/views/admin/districts/index.blade.php
+++ resources/views/admin/districts/index.blade.php
@@ -26,9 +26,9 @@
                                     <td>{{ $district->id }}</td>
                                     <td>{{ $district->name }}</td>
                                     <td>{{ $district->city->name }}</td>
-                                    <tds>#</td>
+                                    <td>#</td>
                                     <td class="text-right">
-                                        <a href="{{ route('adminDistrictsEdit', ['id' => $district->id] ) }}"><button class="btn btn-xs btn-primary">Edit</button></a>
+                                        <a href="{{ route('adminDistrictsEdit', ['id' => $district->id] ) }}"><button class="btn btn-xs btn-primary">Edit</button></a></td>
                                 </tr>
                             @endforeach
                         </tbody>

223877ac35fe37a3f51c6f5765f1cf3047832445
ttc007 <truongthanhcong1909@gmail.com>
2017-05-19 15:10:08 +0700

    feat(products): Product admin delete fubction
    Product admin delete function
    
    see #197

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -40,26 +40,24 @@
                                     <a href="{{ route('adminProductsEdit', ['id' => $product->id] ) }}"><button class="btn btn-xs btn-primary">Edit</button></a>
                                     <a href="" data-toggle="modal" data-target="{{"#".$product->id}}"><button class="btn btn-xs btn-danger">Delete</button></a>
                                         <div id="{{$product->id}}" class="modal fade" role="dialog">
-                                          <div class="modal-dialog">
+                                            <div class="modal-dialog">
 
-                                            <!-- Modal content-->
-                                            <div class="modal-content">
-                                              <div class="modal-header">
-                                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                <h4 class="modal-title">Warning!</h4>
-                                              </div>
-                                              <div class="modal-body">
-                                                <p>Do you sure want to delete this product?</p><br>
-                                                <h4>{{$product->name}}</h4>
-
-                                              </div>
-                                              <div class="modal-footer">
-                                                <a href="{{ route('adminProductsDelete', ['id' => $product->id] ) }}"><button type="button" class="btn btn-danger">Yes</button></a>
-                                                <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
-                                              </div>
+                                                <!-- Modal content-->
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                        <h4 class="modal-title">Warning!</h4>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <p>Do you sure want to delete this product?</p><br>
+                                                        <h4>{{$product->name}}</h4>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <a href="{{ route('adminProductsDelete', ['id' => $product->id] ) }}"><button type="button" class="btn btn-danger">Yes</button></a>
+                                                        <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+                                                    </div>
+                                                </div>
                                             </div>
-
-                                          </div>
                                         </div>
                                 </td>
                             </tr>

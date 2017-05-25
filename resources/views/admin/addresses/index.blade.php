97b27eea323e5a3b9ba4d71d4ad6da0c8a3233e2
npthang <npthang02959@gmail.com>
2017-05-25 19:05:27 +0700

    feat(addresses): Address admin edit page
    see #321

--- resources/views/admin/addresses/index.blade.php
+++ resources/views/admin/addresses/index.blade.php
@@ -38,6 +38,7 @@
                                     <td>{{ $address->country->name }}</td>
                                     <td class="text-right">
                                         <a href="{{ route('adminAddressesEdit', ['id' => $address->id] ) }}"><button class="btn btn-xs btn-primary">Edit</button></a>
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>

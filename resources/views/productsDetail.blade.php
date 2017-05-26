4c70e5192332f062c02c4bf47aafdca7081ff11d
ttc007 <truongthanhcong1909@gmail.com>
2017-05-26 16:32:07 +0700

    feat(home): Create product detail page
    Create product detail page
    
    see #333

--- resources/views/productsDetail.blade.php
+++ resources/views/productsDetail.blade.php
@@ -1,33 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container product-page">
     <div class="row">
         <div class="col-md-12">
-                <div class="panel-body">
-                    <div class="col-md-5">
-                        <img class="img-responsive img-thumbnail" src="{{ asset('upload/'.$product->image) }}" alt="noImage" style="width: 400px;height: 400px">
-                    </div>
-                    <div class="col-md-7">
-                        <h3>{{ $product->name }}</h3>
-                        <div class='border-top'>
-                            <h4>Shop:{{ $product->shop->name }}</h4>
-                            <h3>Price:{{ number_format($product->price) }}VND</h3>
-                            {{Form::open()}}
-                                <div class="form-inline">
-                                {!! Form::label('quantity', 'Quantity') !!}
-                                <div class="form-controls">
+            <div class="col-md-5">
+                <img class="img-responsive img-thumbnail" src="{{ asset('upload/'.$product->image) }}" alt="noImage">
+            </div>
+            <div class="col-md-7">
+                <h3>{{ $product->name }}</h3>
+                <div class='border-top'>
+                    <h4>Shop:{{ $product->shop->name }}</h4>
+                    <h3>Price:{{ number_format($product->price) }}VND</h3>
+                    {{Form::open()}}
+                        <div class="form-inline">
+                            {!! Form::label('quantity', 'Quantity') !!}
+                            <div class="form-controls">
                                 {{ Form::select('qty', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], null, ['class'=>'form-control']) }}
-                                <a href="{{ '#' }}"><button class="btn btn-success">Add to cart <span class="glyphicon glyphicon-shopping-cart"></span></button></a>
-                                </div>
-                                </div>
-                            {{ Form::close() }}
+                                <button class="btn btn-success">Add to cart <span class="glyphicon glyphicon-shopping-cart"></span></button>
+                            </div>
                         </div>
-                        <div class='border-top margin-top'>
-                            <h4>Description</h4>
-                            <p>{{ $product->description }}</p>
-                        </div>
-                    </div>
+                    {{ Form::close() }}
+                </div>
+                <div class='border-top margin-top'>
+                    <h4>Description</h4>
+                    <p>{{ $product->description }}</p>
                 </div>
             </div>
         </div>

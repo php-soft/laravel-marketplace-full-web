66921214fc4dcf9fc1d82ae168e1d8f3530507b8
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-05-28 16:56:34 +0700

    feat(shop): Show product by type
    Show product by type
    
    see #359

--- resources/views/partials/products/product.blade.php
+++ resources/views/partials/products/product.blade.php
@@ -1,4 +1,4 @@
-<div class="thumbnail">
+<div class="thumbnail" style="height: 450px;">
     <a href="{{ route('productsDetail', ['id' => $product->id]) }}"><img class="img-responsive img-thumbnail" src="{{ asset('upload/'.$product->image) }}" alt="noImage" style="width: 300px;"></a>
     <div class="caption">
         <h4>{{ $product->name }}</h4>

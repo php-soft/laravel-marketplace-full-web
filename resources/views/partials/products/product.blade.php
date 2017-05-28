748031ba6922beb2f36cf486c93d4b37ebb0d249
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-05-28 09:00:40 +0700

    feat(shop): Show product by category
    Show product by category
    
    see #343

--- resources/views/partials/products/product.blade.php
+++ resources/views/partials/products/product.blade.php
@@ -3,7 +3,7 @@
         <a href="{{ route('productsDetail', ['id' => $product->id]) }}"><img class="img-responsive img-thumbnail" src="{{ asset('upload/'.$product->image) }}" alt="noImage" style="width: 300px;"></a>
         <div class="caption">
             <h4>{{ $product->name }}</h4>
-            <p>Price:{{ number_format($product->price) }}VND</p>
+            <p>Price: {{ number_format($product->price) }} VND</p>
         </div>
     </div>
 </div>

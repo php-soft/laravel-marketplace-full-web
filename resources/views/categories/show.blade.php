b3dc2006ba18b5e6004620d39d9d5d67577802ae
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-05-28 10:30:00 +0700

    feat(shop): Show product by category
    Show product by category
    
    s #343

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -5,9 +5,11 @@
     <div class="row">
         <div class="col-md-12">
             <h3>{{ $category->name }}</h3>
-            <div class="row">
+            <div class="row" class="col-md-3">
                 @foreach ($category->products(['newest' => true]) as $product)
+                <div class="col-md-3">
                     @include('partials.products.product')
+                </div>
                 @endforeach
             </div>
         </div>
--- resources/views/partials/products/product.blade.php
+++ resources/views/partials/products/product.blade.php
@@ -1,9 +1,7 @@
-<div class="col-md-3">
-    <div class="thumbnail" style="height: 450px">
-        <a href="{{ route('productsDetail', ['id' => $product->id]) }}"><img class="img-responsive img-thumbnail" src="{{ asset('upload/'.$product->image) }}" alt="noImage" style="width: 300px;"></a>
-        <div class="caption">
-            <h4>{{ $product->name }}</h4>
-            <p>Price: {{ number_format($product->price) }} VND</p>
-        </div>
+<div class="thumbnail">
+    <a href="{{ route('productsDetail', ['id' => $product->id]) }}"><img class="img-responsive img-thumbnail" src="{{ asset('upload/'.$product->image) }}" alt="noImage" style="width: 300px;"></a>
+    <div class="caption">
+        <h4>{{ $product->name }}</h4>
+        <p>Price: {{ number_format($product->price) }} VND</p>
     </div>
 </div>

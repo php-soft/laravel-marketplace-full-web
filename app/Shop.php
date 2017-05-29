2f0022b728987129aa18ba67b7fc9a893e4bda6e
BQuyetDev <nguyentruongbaoquyetit@gmail.com>
2017-05-29 16:23:56 +0700

    feat(shops):admin shops edit
    admin shops edit
    
    see #328

--- app/Shop.php
+++ app/Shop.php
@@ -10,55 +10,63 @@ class Shop extends Model
     const STATUS_APPROVED = 1;
     const STATUS_BANNED = 2;
     protected $fillable = [
-        'name',
-        'user_id',
-        'type_id',
-        'city_id',
-        'district_id',
-        'country_id',
-        'description',
-        'image',
-        'status'
+    'name',
+    'user_id',
+    'type_id',
+    'city_id',
+    'district_id',
+    'country_id',
+    'description',
+    'image',
+    'status'
     ];
 
     public function products()
     {
         return $this->hasMany('App\Product');
     }
+
     public function city()
     {
         return $this->belongsTo('App\City');
     }
+
     public function district()
     {
         return $this->belongsTo('App\District');
     }
+
     public function country()
     {
         return $this->belongsTo('App\Country');
     }
+
     public function type()
     {
         return $this->belongsTo('App\Type');
     }
+
     public function shopImages()
     {
         return $this->hasMany('App\ShopImgae');
     }
+
     public function user()
     {
         return $this->belongsTo('App\User');
     }
+    
     public function shopAssignment()
     {
         return $this->belongsTo('App\ShopAssignment');
     }
+
     public static function getStatuses()
     {
         return [
-                self::STATUS_PENDING => 'PENDING',
-                self::STATUS_APPROVED =>'APPROVED',
-                self::STATUS_BANNED => 'BANNED'
-                ];
+        self::STATUS_PENDING => 'PENDING',
+        self::STATUS_APPROVED =>'APPROVED',
+        self::STATUS_BANNED => 'BANNED'
+        ];
     }
 }

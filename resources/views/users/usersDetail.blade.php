1fef807227b5ae2ad85a671802e83112dcd0a6a6
vagrant <haytranngoc@gmail.com>
2017-05-29 16:30:55 +0000

    feat(user): User detail
    User detail
    
    see #381

--- resources/views/users/usersDetail.blade.php
+++ resources/views/users/usersDetail.blade.php
@@ -4,27 +4,54 @@
 <div class="container user-page">
     <div class="row">
         <div class="col-md-12">
-            <div class="col-md-5 user-img">
-                <img class="img-responsive img-thumbnail" src="{{ asset('upload/'.$user->avatar) }}" alt="noImage">
-            </div>
-            <div class="col-md-7 ">
             <div>
-                <h3><span class="label label-default">Information</span></h3>
+                <h3>My Personal Information</h3>
                 <a href="#"><button class="btn btn-xs btn-primary">Edit</button></a>
             </div>
-                <div class='border-top'>
-                    <ul class=" list-group">
-                        <li class="list-group-item"><strong>First Name: </strong>{{ $user->first_name }}</li>
-                        <li class="list-group-item"><strong>Last Name: </strong>{{ $user->last_name }}</li>
-                        <li class="list-group-item"><strong>Date Of Birth: </strong>{{ $user->date_of_birth }}</li>
-                        <li class="list-group-item"><strong>Phone Number: </strong>{{ $user->phone_number }}</li>
-                        <li class="list-group-item"><strong>Email: </strong>{{ $user->email }}</li>
-                        <li class="list-group-item"><strong>Address: </strong>{{ $user->address }}</li>
-                        <li class="list-group-item"><strong>Country: </strong>{{ $user->country->name }}</li>
-                        <li class="list-group-item"><strong>City: </strong>{{ $user->city->name }}</li>
-                        <li class="list-group-item"><strong>District: </strong>{{ $user->district->name }}</li>
-                    </ul>
-                </div>
+            <div class="col-md-4 user-img">
+                <img class="img-responsive img-thumbnail" src="{{ asset('upload/'.$user->avatar) }}" alt="noImage">
+            </div>
+            <div class="col-md-8 ">
+
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        {!! Form::label('first_name', 'First Name') !!}
+                        {{ $user->first_name }}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('last_name', 'Last Name') !!}
+                        {{ $user->last_name }}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('date_of_birth', 'Date Of Birth') !!}
+                        {{ $user->date_of_birth }}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('phone_number', 'Phone Number') !!}
+                        {{ $user->phone_number }}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('email', 'Email') !!}
+                        {{ $user->email }}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('address', 'Address') !!}
+                        {{ $user->address }}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('country', 'Country') !!}
+                        {{ $user->country->name }}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('city', 'City') !!}
+                        {{ $user->city->name }}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('district', 'District') !!}
+                        {{ $user->district->name }}
+                    </div>
+
+                </form>
             </div>
         </div>
     </div>

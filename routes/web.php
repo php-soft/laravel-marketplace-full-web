99bcd807777d55a913ab9027d434465aeece8c23
BQuyetDev <nguyentruongbaoquyetit@gmail.com>
2017-05-30 08:24:19 +0700

    feat(shops):register shops
    register shops
    
    see #399

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,11 @@ Route::post('/order/store', 'OrderController@store')->name('orderStore');
 Route::get('/ajaxCity', 'CityController@select');
 Route::get('/ajaxDistrict', "DistrictController@select");
 
-Route::get('/registerShops/create', 'ShopsController@create')->name('registerShopCreate');
-Route::post('/registerShops', 'ShopsController@store')->name('registerShopStore');
+Route::group([ 'middleware' => ['auth'] ] , function (){
+
+    Route::get('/registerShops/create', 'ShopsController@create')->name('registerShopCreate');
+    Route::post('/registerShops', 'ShopsController@store')->name('registerShopStore');
+});
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
     Route::get('/', 'AdminController@index')->name('admin');

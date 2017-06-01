17f67ab1c435334c003280d927f2e8f2077a66f9
nguyenvantk21 <nguyenvantk21@gmail.com>
2017-06-01 15:40:19 +0700

    feat(shop): Category pagination
    Category Pagination
    
    see #406

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Category;
-use App\Type;
+use App\Product;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -11,7 +11,23 @@ class CategoryController extends Controller
     public function show($id)
     {
         $category = Category::findOrFail($id);
+        $products = Product::where('category_id', '=', $id)->paginate(8);
+
+        $has_page = $products->currentPage() - 1;
+        $prev_current = 1;
+        if ($has_page > 3) {
+            $prev_current = $products->currentPage() - 3;
+        }
+
+        $remain_page = $products->lastPage() - $products->currentPage();
+        $next_current = $products->lastPage();
+        if ($remain_page > 3) {
+            $next_current = $products->currentPage() + 3;
+        }
         return view('categories.show')
-            ->with('category', $category);
+            ->with('category', $category)
+            ->with('products', $products)
+            ->with('prev_current', $prev_current)
+            ->with('next_current', $next_current);
     }
 }
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -6,12 +6,23 @@
         <div class="col-md-12">
             <h3>{{ $category->name }}</h3>
             <div class="row" class="col-md-3">
-                @foreach ($category->products(['newest' => true]) as $product)
+                @foreach ($products as $product)
                 <div class="col-md-3">
                     @include('partials.products.product')
                 </div>
                 @endforeach
             </div>
+        </div>
+        <div>
+            <ul class="pagination">
+              <li><a href="{{ $products->url(1) }}">First</a></li>
+              <li><a href="{{ $products->url($prev_current) }}">&laquo;</a></li>
+              <li><a href="{{ $products->previousPageUrl() }}">Prev</a></li>
+              <li class="active"><a href="{{ $products->currentPage() }}">{{ $products->currentPage() }}</a></li>
+              <li><a href="{{ $products->nextPageUrl() }}">Next</a></li>
+              <li><a href="{{ $products->url($next_current) }}">&raquo;</a></li>
+              <li><a href="{{ $products->url($products->lastPage()) }}">Last</a></li>
+            </ul>
         </div>
     </div>
 </div>

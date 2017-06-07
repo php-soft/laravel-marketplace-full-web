b9067b7f73571747b39198487fa61a6f6aa6e1ec
npthang <npthang02959@gmail.com>
2017-06-08 02:28:58 +0700

    feat(cities): Add Faker PHP library
    see #412

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,18 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Country::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name
+    ];
+});
+
+$factory->define(App\City::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'country_id' => function () {
+            return factory(App\Country::class)->create()->id;
+        }
+    ];
+});
--- database/seeds/CitiesTableSeeder.php
+++ database/seeds/CitiesTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use App\City;
+use App\Country;
 
 class CitiesTableSeeder extends Seeder
 {
@@ -13,13 +14,24 @@ class CitiesTableSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker\Factory::create();
+       /* $faker = Faker\Factory::create();
         foreach(range(1,50) as $index)
         {
             City::create([
                 'name' => $faker->name,
-                'country_id' => $faker->randomNumber
+                'country_id' => function () {
+                    return factory(App\Country::class)->create()->id;
+                }
             ]);
-        }
+        }*/
+
+        $faker = Faker\Factory::create();
+        factory(App\City::class, 50)
+        ->create([
+            'name' => $faker->name,
+            'country_id' => function () {
+                return factory(App\Country::class)->create()->id;
+            }
+        ]);
     }
 }

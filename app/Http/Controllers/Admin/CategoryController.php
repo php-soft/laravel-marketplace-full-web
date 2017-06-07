70582987e39f5bd66a4f81e80348d4a818c57c59
npthang <npthang02959@gmail.com>
2017-06-08 01:09:45 +0700

    feat(categories): Admin pagination
    see #420

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        $categories = Category::all();
+        $categories = Category::paginate(10);
         return view('admin.categories.index')->with('categories', $categories);
     }
 
